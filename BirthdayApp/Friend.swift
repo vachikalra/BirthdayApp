c53ff564fc886027fcb8824e50e0aca2720cdeed
Vachi Kalra <vachikalra@gmail.com>
2025-07-25 10:31:36 -0700

    Added Friend class + UI to Add Friend

---
+++ BirthdayApp/Friend.swift
@@ -0,0 +1,18 @@
+//
+//  Friend.swift
+//  BirthdayApp
+//
+//  Created by Vachi Kalra on 7/25/25.
+//
+
+import Foundation
+
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date){
+        self.name = name
+        self.birthday = birthday
+    }
+}
